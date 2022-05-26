dfa780d9f96f8673e77c921f34db7c0931ac2038
marcoslima12 <marcosantonio-10@live.com>
2022-05-25 22:44:57 -0300

    feat(feature/Feedbacks_CRUD): Deleting files related to User database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,8 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import FeedbackController from '@controllers/FeedbackController';
 
 const routes = express.Router();
-const userController = new UserController();
 const feedbackController = new FeedbackController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/feedback', feedbackController.create);
 routes.get('/feedback', feedbackController.get);
