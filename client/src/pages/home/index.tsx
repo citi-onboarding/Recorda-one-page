5b71a9f45518abb7f0742af86a6055c1797dc2ad
marcoslima12 <marcosantonio-10@live.com>
2022-05-29 09:12:54 -0300

    feat(feature/secao_inicial): updating

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { HomeContainer } from './styles';
 import { LogoCITi } from '../../assets';
-import { InialSection } from "../inicitalSection";
 
 
 export const Home: React.FC = () => {
     return (
         <HomeContainer>
-            <InialSection></InialSection>
         </HomeContainer>
     );
 }
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,7 +1,5 @@
 import { Home } from './home';
-import { Dados } from './Dados';
-import { InialSection } from './inicitalSection';
 
 export {
-    Home, Dados, InialSection
+    Home
 };
