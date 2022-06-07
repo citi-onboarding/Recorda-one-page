c702cf280abd6d955ec5cfb57decddb38a056046
Chubilu01 <5117.joaopedro@gmail.com>
2022-06-07 18:38:22 -0300

    just some adjustments

--- client/src/pages/news/index.tsx
+++ client/src/pages/news/index.tsx
@@ -7,8 +7,8 @@ export const NewsSection: React.ElementType = () => {
         <NewsContainer>
             <NewsTitle><NewsTitleImage src={HearthImage} alt="Emoji de coração" /> Entendendo melhor <NewsTitleImage src={HearthImage} alt="Emoji de coração" /></NewsTitle>
             <CardsContainer>
-                <News newsName={""} newsText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur sagittis volutpat. Aliquam nisl nibh, tincidunt at mi quis, finibus fermentum enim. Aliquam ac ultrices lectus. Fusce non luctus sapien. Integer at fringilla ex. Aenean eu enim eget nulla accumsan maximus. Mauris viverra ut libero mattis mattis. Integer imperdiet neque sed condimentum accumsan."} newsLink={""}/>
-                <News newsName={""} newsText={"Suspendisse venenatis interdum vehicula. Morbi mattis mauris et pretium vestibulum. Nullam interdum nisi quis tempor accumsan. Praesent bibendum pellentesque dolor, sed faucibus massa mollis id. Vestibulum a justo eu nulla malesuada euismod. Suspendisse a tellus ut turpis suscipit sodales sit amet sit amet risus. Vivamus et libero elit."} newsLink={""}/>
+                <News newsName={"News1"} newsText={"À medida que a demência progride, cada pessoa encontrará sua própria maneira de lidar, reagir e se adaptar às mudanças que ela traz. Desenvolver essas estratégias de enfrentamento pode ser um processo gradual e subconsciente."} newsLink={""}/>
+                <News newsName={"News2"} newsText={"Esta ficha informativa fornece algumas estratégias práticas para lidar com os problemas de comportamento preocupantes e as dificuldades de comunicação frequentemente encontradas ao cuidar de uma pessoa com demência."} newsLink={""}/>
             </CardsContainer>
         </NewsContainer>
     );
