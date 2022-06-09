1bc040005e2d64a8999d1d3d268ed69761e415cb
Chubilu01 <5117.joaopedro@gmail.com>
2022-06-08 21:50:26 -0300

    feat(feedback_integration): we have a problem

--- client/src/pages/feedback/index.tsx
+++ client/src/pages/feedback/index.tsx
@@ -1,23 +1,41 @@
 import { Feedback } from "../../components/feedback_card/FeedbackCard";
 import { SeniorsImage, FeedbackTitleEmoji } from "../../assets";
 import { FeedbackContainer, FeedbackTitle, UpperFeedbacks, LowerFeedbacks, FeedbackCardsDiv, FeedbackTitleImage, FeedbackCardImg } from "./styles";
+import api from "../../services/api";
+import { useState, useEffect } from "react";
+
+type FeedbackData = {
+    personName: string,
+    feedbackText: string
+};
 
 export const FeedbackSection: React.ElementType = () => {
+
+    const [feedback, setFeedback] = useState<FeedbackData[]>([]);
+    const getFeedback = async () => {
+        const response = await api.get('feedback');
+        setFeedback(response.data);
+    };
+
+    useEffect(() => {
+        getFeedback();
+    }, []);
+
     return (
         <FeedbackContainer>
             <FeedbackTitle>O que as famílias estão falando sobre nós?<FeedbackTitleImage src={FeedbackTitleEmoji} alt="Emoji do título da seção de feedbacks" /></FeedbackTitle>
             <FeedbackCardsDiv>
                 <UpperFeedbacks>
-                    <Feedback userName={"Sofia Diniz"} userText={"Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare. Fusce commodo lacus et neque luctus ornare."} />
-                    <Feedback userName={"Sofia Diniz"} userText={"Fusce commodo lacus et neque luctus ornare."} />
+                    <Feedback userName={feedback[0].personName} userText={feedback[0].feedbackText} />
+                    <Feedback userName={feedback[1].personName} userText={feedback[1].feedbackText} />
                     <FeedbackCardImg src={SeniorsImage} alt="Imagem dos idosos" />
                 </UpperFeedbacks>
                 <LowerFeedbacks>
-                    <Feedback userName={"Sofia Diniz"} userText={"Fusce commodo lacus et neque luctus ornare."} />
-                    <Feedback userName={"Sofia Diniz"} userText={"Fusce commodo lacus et neque luctus ornare."} />
-                    <Feedback userName={"Sofia Diniz"} userText={"Fusce commodo lacus et neque luctus ornare."} />
+                    <Feedback userName={feedback[2].personName} userText={feedback[2].feedbackText} />
+                    <Feedback userName={feedback[3].personName} userText={feedback[3].feedbackText} />
+                    <Feedback userName={feedback[4].personName} userText={feedback[4].feedbackText} />
                 </LowerFeedbacks>
             </FeedbackCardsDiv>
         </FeedbackContainer>
     );
-}
+};
