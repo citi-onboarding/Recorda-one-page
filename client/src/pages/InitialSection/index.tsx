ed66045716ef03e774db45b6e21ab04dd0033a24
marcoslima12 <marcosantonio-10@live.com>
2022-06-08 22:26:12 -0300

    feat(feature/initial_section_integration): Finilized the integration. Still have to fix the key value problem

--- client/src/pages/InitialSection/index.tsx
+++ client/src/pages/InitialSection/index.tsx
@@ -1,3 +1,6 @@
+import api from "../../services/api";
+import { useState, useEffect } from "react";
+
 import {
     InitialContainer,
     FraseDeEfeito,
@@ -20,9 +23,34 @@ import {
     DownloadVector,
 } from "../../assets";
 
-import api from "../../services/api";
+type InitialData = {
+    id: number,
+    info1Title: string,
+    info1Subtitle: string,
+    info2Title: string,
+    info2Subtitle: string,
+    info3Title: string,
+    info3Subtitle: string,
+}
 
 export const Initial: React.ElementType = () => {
+
+    const [data, setData] = useState<InitialData[]>([]);
+
+
+    async function getData() {
+        try {
+            const response = await api.get(`firstsection`)
+            console.log(response.data);
+            setData(response.data);
+        } catch (error) {
+            console.log('CANT')
+            alert("cant connect")
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
 
 
     return (
@@ -30,8 +58,8 @@ export const Initial: React.ElementType = () => {
             <TopContainer>
                 <FraseDeEfeito>Recordar é amar</FraseDeEfeito>
                 <Paragrafo>
-                    Demonstre todo seu amor pelos idosos na sua vida com o<br/> aplicativo Recorda.
-                    Ajude-os a administrar seus remédios e<br/> histórico médico de maneira simples e organizada.
+                    Demonstre todo seu amor pelos idosos na sua vida com o<br /> aplicativo Recorda.
+                    Ajude-os a administrar seus remédios e<br /> histórico médico de maneira simples e organizada.
                 </Paragrafo>
                 <ButtonDownload>
                     <Image src={DownloadVector} id="DownloadImageVector"></Image>
@@ -39,10 +67,17 @@ export const Initial: React.ElementType = () => {
                 <Image src={OldPeopleImage} id="OldPeopleImage" alt="Ilustração de Grupo de Pessoas Idosas" />
             </TopContainer>
             <BottonContainer>
-                <DataContainer>
-                    <DataContent id="Data1"><Dados numero={154} titulo={"FAMILIAS ASSISTIDAS"} /></DataContent>
-                    <DataContent id="Data2"><Dados numero={980} titulo={"MODEL SENTENCE"} /></DataContent>
-                    <DataContent id="Data3"><Dados numero={253} titulo={"STANDARD CHUNK"} /></DataContent>
+                <DataContainer>{
+                    data.map(
+                        (data) => [
+                            <>
+                                <DataContent id="Data1"><Dados numero={data.info1Title} titulo={data.info1Subtitle} /></DataContent>
+                                <DataContent id="Data2"><Dados numero={data.info2Title} titulo={data.info2Subtitle} /></DataContent>
+                                <DataContent id="Data3"><Dados numero={data.info3Title} titulo={data.info3Subtitle} /></DataContent>
+                            </>
+                        ]
+                    )
+                }
                 </DataContainer>
                 <Image src={CellphoneImage} id="ScreenPhone" alt="Ilustração De Telas do Aplicatico" />
             </BottonContainer>
