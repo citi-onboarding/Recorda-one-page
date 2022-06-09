fd80c99c309f8e098a5d25edc5d9745bb3913427
marcoslima12 <marcosantonio-10@live.com>
2022-06-09 01:24:33 -0300

    feat(feature/footer-integration): Finilized the footer integration

--- client/src/pages/Footer/index.tsx
+++ client/src/pages/Footer/index.tsx
@@ -1,4 +1,7 @@
-import { 
+import api from "../../services/api";
+import { useState, useEffect } from "react";
+
+import {
   Citi,
   HeartFooter,
   LogoFooter
@@ -12,34 +15,62 @@ import {
   MediaContainer,
   BottonContainer,
   Container,
-  Image, 
+  Image,
 } from "./styles";
 
 import {
   MediaContent,
 } from "../../components";
 
+type FooterData = {
+  socialMedia1: string,
+  socialMedia2: string,
+  socialMedia3: string,
+}
+
 
 export const FooterSection: React.ElementType = () => {
+
+  const [links, setLinks] = useState<FooterData[]>([])
+  async function getLinks() {
+    try {
+      const response = await api.get(`footer`);
+      setLinks(response.data);
+    }
+    catch (error) {
+      console.log("Error conecting to data base");
+    }
+  }
+
+  useEffect(() => {
+    getLinks()
+  }, [])
+
   return (
     <Footer>
       <Container>
-          <TopContainer>
-            <Image src={LogoFooter} alt="Logo da Recorda One" id="FooterLogo" />
-            <MediaContainer>
-              <p id="TextFollowUs">Nos siga nas redes sociais</p>
-              <SocialMediaContainer>
-                <MediaContent socialMediaLink={"#"} image={HeartFooter} imageName={"Instagram icon"}></MediaContent>
-                <MediaContent socialMediaLink={"#"} image={HeartFooter} imageName={"LinkedIn icon"}></MediaContent>
-                <MediaContent socialMediaLink={"#"} image={HeartFooter} imageName={"Github Icon"}></MediaContent>
-              </SocialMediaContainer>
-            </MediaContainer>
-          </TopContainer>
-          <BottonContainer>
-            <hr />
-            <p> Made with &lt;/&gt;  and <Image src={HeartFooter} alt="Heart Icon" className="FooterIcon" /> by <Image src={Citi} alt="Citi Icon" id="TextFooter" className="FooterIcon" /> </p>
-          </BottonContainer>
-      </Container>   
+        <TopContainer>
+          <Image src={LogoFooter} alt="Logo da Recorda One" id="FooterLogo" />
+          <MediaContainer>
+            <p id="TextFollowUs">Nos siga nas redes sociais</p>
+            {
+              links.map(
+                (link) => [
+                  <SocialMediaContainer>
+                    <MediaContent socialMediaLink={link.socialMedia1} image={HeartFooter} imageName={"Instagram icon"}></MediaContent>
+                    <MediaContent socialMediaLink={link.socialMedia2} image={HeartFooter} imageName={"LinkedIn icon"}></MediaContent>
+                    <MediaContent socialMediaLink={link.socialMedia3} image={HeartFooter} imageName={"Github Icon"}></MediaContent>
+                  </SocialMediaContainer>
+                ]
+              )
+            }
+          </MediaContainer>
+        </TopContainer>
+        <BottonContainer>
+          <hr />
+          <p> Made with &lt;/&gt;  and <Image src={HeartFooter} alt="Heart Icon" className="FooterIcon" /> by <Image src={Citi} alt="Citi Icon" id="TextFooter" className="FooterIcon" /> </p>
+        </BottonContainer>
+      </Container>
     </Footer>
   );
 }
